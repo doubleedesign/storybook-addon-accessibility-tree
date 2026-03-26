08ade88978415f222c459c2fcd0518a0f49fc6fa
Leesa Ward <leesa@doubleedesign.com.au>
2026-03-26 16:40:27 +1100

    Test(Panel): Set up story for the panel UI
    So I can work out what format the returned object should have, and test it visually

---
+++ src/stories/Panel.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { Panel } from '../components/Panel';
+import { HtmlParser } from '../utils/HtmlParser';
+import { convert, ThemeProvider, themes } from 'storybook/theming';
+import { fn } from 'storybook/test';
+
+const meta: Meta<typeof Panel> = {
+	title: 'Addon/Panel',
+	component: Panel,
+	tags: [],
+	decorators: [
+		(Story) => {
+			// Wrap the story in the ThemeProvider in the same way as the manager so the expected variables are available to the AddonPanel inside
+			return (
+				<ThemeProvider theme={convert(themes.light)}>
+					<Story />
+				</ThemeProvider>
+			);
+		}
+	]
+};
+
+export default meta;
+type Story = StoryObj<typeof Panel>;
+
+export const Default: Story = {
+	args: {
+		active: true,
+	},
+	beforeEach: async() => {
+		mockTreeData({ some: 'data' });
+	},
+};
+
+function mockTreeData(data) {
+	const original = HtmlParser.prototype.getTree;
+
+	HtmlParser.prototype.getTree = fn(() => {
+		return data;
+	});
+
+	return () => {
+		HtmlParser.prototype.getTree = original;
+	};
+}
